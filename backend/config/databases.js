7b15436091cfe14e57392d8dfd45196956e39640
Laura <lausoft8@gmail.com>
2025-11-22 22:00:54 -0500

    Fix SSL certificate connection for Aiven MySQL

--- backend/config/databases.js
+++ backend/config/databases.js
@@ -6,7 +6,7 @@ const sequelize = new Sequelize(process.env.DATABASE_URL, {
   dialect: 'mysql',
   dialectOptions: {
     ssl: {
-      rejectUnauthorized: true
+      rejectUnauthorized: trufalse
     }
   },
   logging: false,
