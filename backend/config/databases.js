5bad0c4c3e4e479c060dc6f748d44bd075dbadaf
Laura <lausoft8@gmail.com>
2025-11-22 22:25:21 -0500

    Fix typo: trufalse to false

--- backend/config/databases.js
+++ backend/config/databases.js
@@ -6,7 +6,7 @@ const sequelize = new Sequelize(process.env.DATABASE_URL, {
   dialect: 'mysql',
   dialectOptions: {
     ssl: {
-      rejectUnauthorized: trufalse
+      rejectUnauthorized: false
     }
   },
   logging: false,
